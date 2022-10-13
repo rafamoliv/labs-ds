a18b445e3888ab0812cbb4b2615e0022ce62ed0b
rafael.oliveira <rafael.oliveira@psafe.com>
2022-10-13 20:26:24 -0300

    fix: fixing tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,13 +11,19 @@ module.exports = {
       '2xl': 32
     },
     colors: {
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7c7c8a',
-      'gray-200': '#c4c4cc',
-      'gray-100': '#e1e1e6',
-      'cyan-500': '#81d8f7',
-      'cyan-300': '#9be1fb'
+      black: '#000',
+      white: '#fff',
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+      cyan: {
+        500: '#81d8f7',
+        300: '#9be1fb'
+      }
     },
     extend: {
       fontFamily: {
